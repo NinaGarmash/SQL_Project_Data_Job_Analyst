7741e08a669ac7044e67bfc742634e87d9f4929a
NinaGarmash <ninagarmash@gmail.com>
2024-03-22 16:41:35 +0200

    upload sql code

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,96 @@
+/*
+Question: What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst.
+- Focus on all job postings.
+- Why? Retrieves the top 5 skills with the highest demand in the job market, 
+    providing insights into the most valuable skills for job seekers.
+- Do the same for jobs in Israel
+*/
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_work_from_home = True 
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5;
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_location LIKE '%Israel%' 
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5;
+
+
+
+/*
+Here's the breakdown of the most demanded skills for data analysts in 2023
+SQL and Excel remain fundamental, emphasizing the need for strong foundational skills in data processing and spreadsheet manipulation.
+Programming and Visualization Tools like Python, Tableau, and Power BI are essential, pointing towards the increasing importance of technical skills in data storytelling and decision support.
+In Israel the top 5 skills are the same, but their order in the demand hierarchy is different:
+sql-python-tableau-excel-power bi. So in Israel excell is only the 4th skill(not the 2nd), python and tableau have surpassed it.
+
+[
+  {
+    "skills": "sql",
+    "demand_count": "7291"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "4611"
+  },
+  {
+    "skills": "python",
+    "demand_count": "4330"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "3745"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "2609"
+  }
+]
+
+For Israel:
+[
+  {
+    "skills": "sql",
+    "demand_count": "628"
+  },
+  {
+    "skills": "python",
+    "demand_count": "395"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "243"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "171"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "118"
+  }
+]
+*/
